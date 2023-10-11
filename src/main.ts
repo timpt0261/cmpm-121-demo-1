83d8be5ae488a577615462938dc07e8c6082e14e
timpt0261 <rchavez0215@gmail.com>
2023-10-11 16:21:45 -0700

    Completed Step 5

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ const button = document.createElement("button");
 
 button.title = "button";
 button.innerHTML = "💧";
+
 app.append(button);
 
 const divider = document.createElement("div");
@@ -21,19 +22,62 @@ divider.id = "counter";
 
 app.append(divider);
 
+const upgrade_a = document.createElement("button");
+upgrade_a.id = "upgrade_a";
+upgrade_a.innerHTML = "strengthen throw";
+upgrade_a.disabled = true;
+
+app.append(upgrade_a);
+
+// const upgrade_b = document.createElement("button");
+// upgrade_b.id = "upgrade_b";
+// upgrade_b.innerHTML = "strengthen throw";
+// upgrade_b.disabled = true;
+
+// app.append(upgrade_b);
+
+// const upgrade_c = document.createElement("button");
+// upgrade_c.id = "upgrade_c";
+// upgrade_c.innerHTML = "strengthen throw";
+// upgrade_c.disabled = true;
+
+// app.append(upgrade_c);
+
 let counter = 0;
 let start: number | undefined = undefined;
-let progress: number | undefined = undefined;
-const updateDelay = 1 / 60; // 60 units per second
+const growthRate = 1; // modify to change frame rate
+
+button.addEventListener("click", () => {
+  console.log("Throw");
+  counter++;
+});
+
+upgrade_a.addEventListener("click", () => {
+  console.log("Strength A");
+  counter -= 10;
+});
+
+// upgrade_b.addEventListener("click", () => {
+//   console.log("Strength B");
+//   counter -= 100;
+//   updateDelay += 2.0;
+// });
+
+// upgrade_c.addEventListener("click", () => {
+//   console.log("Strength C");
+//   counter -= 1000;
+//   updateDelay += 50;
+// });
 
 function tick(timestamp: number) {
-  const diff = progress! - start!;
-  if (!start || diff >= updateDelay) {
-    start = timestamp / 1000;
-    divider.innerHTML = `${counter} Waves`;
-    counter++;
-  }
-  progress = timestamp / 1000;
+  console.log(`counter : ${counter}`);
+  upgrade_a.disabled = counter >= 10 ? false : true;
+  // upgrade_b.disabled = counter >= 100 ? false : true;
+  // upgrade_c.disabled = counter >= 1000 ? false : true;
+  if (!start) start = timestamp / 1000;
+  counter += growthRate * (timestamp / 1000 - start!);
+  divider.innerHTML = ` waves ${counter.toFixed(3)}`;
+  start = timestamp / 1000;
 
   requestAnimationFrame(tick);
 }
