7f48ecf111aac2f33444c3db113efe9d1804fe01
timpt0261 <rchavez0215@gmail.com>
2023-10-12 17:18:22 -0700

    Step 7 complete

--- src/main.ts
+++ src/main.ts
@@ -29,20 +29,18 @@ app.append(display_growth_rate);
 
 const upgrade_a = document.createElement("button");
 upgrade_a.id = "upgrade_a";
-upgrade_a.innerHTML = "Improve Hull ";
+upgrade_a.disabled = true;
 
 app.append(upgrade_a);
 
 const upgrade_b = document.createElement("button");
 upgrade_b.id = "upgrade_b";
-upgrade_b.innerHTML = "Improve Sail";
 upgrade_b.disabled = true;
 
 app.append(upgrade_b);
 
 const upgrade_c = document.createElement("button");
 upgrade_c.id = "upgrade_c";
-upgrade_c.innerHTML = "Improve MotorBoat";
 upgrade_c.disabled = true;
 
 app.append(upgrade_c);
@@ -50,6 +48,21 @@ app.append(upgrade_c);
 let counter = 0;
 let start: number | undefined = undefined;
 let growthRate = 0;
+
+const constant_a = 10;
+const growth_factor_a = 1.15;
+let time_constant_a = 0;
+let cost_a = constant_a * growth_factor_a ** time_constant_a;
+
+const constant_b = 100;
+const growth_factor_b = 1.15;
+let time_constant_b = 0;
+let cost_b = constant_b * growth_factor_b ** time_constant_b;
+
+const constant_c = 1000;
+const growth_factor_c = 1.15;
+let time_constant_c = 0;
+let cost_c = constant_c * growth_factor_c ** time_constant_c;
 
 button.addEventListener("click", () => {
   console.log("Throw Rocks");
@@ -60,32 +73,54 @@ upgrade_a.addEventListener("click", () => {
   console.log("Improve Body");
   counter -= 10;
   growthRate += 0.1;
+  time_constant_a++;
+  cost_a = constant_a * growth_factor_a ** time_constant_a;
 });
 
 upgrade_b.addEventListener("click", () => {
   console.log("Improve Sail");
   counter -= 100;
   growthRate += 2.0;
+  time_constant_b++;
+  cost_b = constant_b * growth_factor_b ** time_constant_b;
 });
 
 upgrade_c.addEventListener("click", () => {
   console.log("Improve Engine");
   counter -= 1000;
   growthRate += 50;
+  time_constant_c++;
+  cost_c = constant_c * growth_factor_c ** time_constant_c;
 });
+1;
 
 function tick(timestamp: number) {
   console.log(`counter : ${counter}`);
-  upgrade_a.disabled = counter >= 10 ? false : true;
-  upgrade_b.disabled = counter >= 100 ? false : true;
-  upgrade_c.disabled = counter >= 1000 ? false : true;
+
+  upgrade_a.disabled = counter >= constant_a ? false : true;
+  upgrade_b.disabled = counter >= cost_b ? false : true;
+  upgrade_c.disabled = counter >= cost_c ? false : true;
 
   if (!start) start = timestamp / 1000;
-  counter += growthRate * (timestamp / 1000 - start!);
+  const elapsed = timestamp / 1000 - start!;
+
+  console.log(` elapsed : ${elapsed}`);
+  counter += growthRate * elapsed;
   divider.innerHTML = `${counter.toFixed(3)} knots per hour`;
   display_growth_rate.innerHTML = ` current growth rate ${growthRate.toFixed(
     1,
   )} at knots per hour `;
+
+  upgrade_a.innerHTML = `Improve Hull ${cost_a.toFixed(
+    1,
+  )} (${time_constant_a})`;
+  upgrade_b.innerHTML = `Improve Sail ${cost_b.toFixed(
+    1,
+  )} (${time_constant_b})`;
+  upgrade_c.innerHTML = `Improve MotorBoat ${cost_c.toFixed(
+    1,
+  )} (${time_constant_c})`;
+
   start = timestamp / 1000;
 
   requestAnimationFrame(tick);
